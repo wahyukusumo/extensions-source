311a50be27f549b3d68f98aeaccb4a19c1bb3f63
Wahyu Kusumo <dev@wahyukusumo.com>
2026-07-15 03:08:34 +0700

    feat: deleted unused files

--- src/en/qiscans/src/eu/kanade/tachiyomi/extension/en/qiscans/GenreDto.kt
+++
@@ -1,9 +0,0 @@
-package eu.kanade.tachiyomi.extension.en.qiscans
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-class GenreDto(val id: Int, val name: String)
-
-@Serializable
-class PageDto(val url: String, val order: Int)
